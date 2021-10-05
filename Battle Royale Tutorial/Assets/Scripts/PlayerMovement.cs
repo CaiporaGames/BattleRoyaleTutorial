28e754f0a0b54e41b10d709a56b72dc510e05efd
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-10-05 14:59:21 -0300

    Movimento da cabeca
    Fiz o script do movimento da cabeca

--- Battle Royale Tutorial/Assets/Scripts/PlayerMovement.cs
+++ Battle Royale Tutorial/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float sensitivity;
+    [SerializeField] float minView;
+    [SerializeField] float maxView;
+    [SerializeField] Transform cameraTransform;
+
 
 
     CharacterController cc;
     Vector3 move;
+    float mouseX;
+    float mouseY;
 
 
     // Start is called before the first frame update
@@ -22,12 +29,24 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        Look();
     }
 
     void MovePlayer()
     {
         move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         move = Vector3.ClampMagnitude(move, 1);
+        move = transform.TransformDirection(move);
         cc.SimpleMove(speed * move);
+    }
+
+    void Look()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        transform.Rotate(0, mouseX, 0);
+
+        mouseY -= Input.GetAxis("Mouse Y") * sensitivity;
+        mouseY = Mathf.Clamp(mouseY, minView, maxView);
+        cameraTransform.localRotation = Quaternion.Euler(mouseY, 0,0);
     }
 }
