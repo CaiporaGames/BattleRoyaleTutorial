8cad5c34134f0b0d0f60e25aab0d3f7770ca28c4
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-10-05 13:57:57 -0300

    02- Player Movement
    Criamos o movimento do player.

--- Battle Royale Tutorial/Assets/Scripts/PlayerMovement.cs
+++ Battle Royale Tutorial/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float speed;
+
+
+    CharacterController cc;
+    Vector3 move;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        cc = GetComponent<CharacterController>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1);
+        cc.SimpleMove(speed * move);
     }
 }
